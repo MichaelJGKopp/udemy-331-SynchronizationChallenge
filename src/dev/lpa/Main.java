905f47a15337beeb533a13e1a65ef615cd025673
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-09-21 10:45:06 +0200

    change thread management to ExecutorService

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,37 +1,33 @@
 package dev.lpa;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
   public static void main(String[] args) {
 
     Random random = new Random();
-    ShoeWarehouse shoeWarehouse = new ShoeWarehouse();
+    ShoeWarehouse shoeWarehouse = new ShoeWarehouse(2);
 
-    Thread producer = new Thread(() -> {
-      for (int i = 0; i < 10; i++) {
+    Runnable producerRunnable = () -> {
+      for (int i = 0; i < 15; i++) {
         shoeWarehouse.receiveOrder(new Order(
           random.nextLong(1000000, 9999999),
           ShoeWarehouse.PRODUCT_LIST[random.nextInt(0,3)],
           random.nextInt(0, 10)));
       }
-    });
-    producer.start();
+    };
+    ExecutorService producer = Executors.newSingleThreadExecutor(
+      r -> new Thread(r, "\u001B[33mProducer"));
+    producer.execute(producerRunnable);
 
-    for (int i = 0; i < 2; i++) {
-      Thread consumer = new Thread(() -> {
-        for (int j = 0; j < 5; j++) {
-          shoeWarehouse.fulfillOrder();
-        }
-      });
-      consumer.start();
+    for (int i = 0; i < 15; i++) {
+      shoeWarehouse.consumerFulfillOrder();
     }
 
-    try {
-      Thread.sleep(3000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    producer.shutdown();
+    shoeWarehouse.consumerShutdown();
   }
 }
--- src/dev/lpa/ShoeWarehouse.java
+++ src/dev/lpa/ShoeWarehouse.java
@@ -2,8 +2,12 @@ package dev.lpa;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ShoeWarehouse {
+
+  private final ExecutorService consumer;
 
   public final static String[] PRODUCT_LIST = {
     "Shoe1",
@@ -12,8 +16,9 @@ public class ShoeWarehouse {
   };
   private List<Order> orders;
 
-  public ShoeWarehouse() {
+  public ShoeWarehouse(int threadNumber) {
     this.orders = new ArrayList<>();
+    this.consumer = Executors.newFixedThreadPool(threadNumber);
   }
 
   public synchronized void receiveOrder(Order order) {
@@ -26,8 +31,12 @@ public class ShoeWarehouse {
       }
     }
     orders.add(order);
-    System.out.println("Add" + order);
+    System.out.println(Thread.currentThread().getName() + " adds" + order);
     notifyAll();
+  }
+
+  public synchronized void consumerFulfillOrder() {
+      consumer.submit(this::fulfillOrder);
   }
 
   public synchronized Order fulfillOrder() {
@@ -40,9 +49,13 @@ public class ShoeWarehouse {
       }
     }
     Order order = orders.removeFirst();
-    System.out.println(Thread.currentThread().getName() + " Fulfill " + order);
+    System.out.println("\u001B[34m" + Thread.currentThread().getName() + " fulfills " + order);
     notifyAll();
     return order;
+  }
+
+  public void consumerShutdown() {
+    consumer.shutdown();
   }
 
 }
